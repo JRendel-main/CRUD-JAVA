161387a23eb1248b2513a8d8c67c903784b2f557
0x41gawor <41gawor@gmail.com>
2022-01-27 00:50:49 +0100

    feat(controller): add get jdcb connection method

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -5,6 +5,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.sql.*;
+
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -21,5 +24,16 @@ public class HelloController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public Connection getConnection() {
+        Connection connection;
+            try {
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/library", "root", "ejek");
+                return connection;
+            } catch (Exception ex) {
+                System.out.println("Error: " + ex.getMessage());
+                return null;
+            }
     }
 }
