b88685f04db10921067fcb9109c40d552c08b5a5
0x41gawor <41gawor@gmail.com>
2022-01-27 01:01:53 +0100

    feat(controller): add list CRUD method

--- src/main/java/com/example/hello/HelloController.java
+++ src/main/java/com/example/hello/HelloController.java
@@ -1,9 +1,14 @@
 package com.example.hello;
 
+import com.example.hello.model.BookModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.*;
 
@@ -15,6 +20,21 @@ public class HelloController implements Initializable {
 
     @FXML
     private TextField textField_Id;
+
+    @FXML
+    private TableColumn tableColumn_id;
+
+    @FXML
+    private TableColumn tableColumn_title;
+
+    @FXML
+    private TableColumn tableColumn_author;
+
+    @FXML
+    private TableColumn tableColumn_year;
+
+    @FXML
+    private TableColumn tableColumn_pages;
 
     @FXML
     protected void onInsertButtonClick() {
@@ -35,5 +55,39 @@ public class HelloController implements Initializable {
                 System.out.println("Error: " + ex.getMessage());
                 return null;
             }
+    }
+
+    public ObservableList<BookModel> list() {
+        ObservableList<BookModel> list = FXCollections.observableArrayList();
+        Connection connection = getConnection();
+        String query = "SELECT * FROM books";
+        Statement statement;
+        ResultSet resultSet;
+
+        try {
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+
+            BookModel model;
+            while (resultSet.next()) {
+                model = new BookModel(resultSet.getInt("id"),
+                        resultSet.getString("title"),
+                        resultSet.getString("author"),
+                        resultSet.getInt("year"),
+                        resultSet.getInt("pages"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+    }
+
+    public void showBooks() {
+        ObservableList<BookModel> list = list();
+        tableColumn_id.setCellValueFactory(new PropertyValueFactory<BookModel, Integer>("id"));
+        tableColumn_title.setCellValueFactory(new PropertyValueFactory<BookModel, String>("title"));
+        tableColumn_author.setCellValueFactory(new PropertyValueFactory<BookModel, String>("author"));
+        tableColumn_year.setCellValueFactory(new PropertyValueFactory<BookModel, Integer>("year"));
+        tableColumn_pages.setCellValueFactory(new PropertyValueFactory<BookModel, Integer>("pages"));
     }
 }
