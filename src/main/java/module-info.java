e3777ffc985a52e7d3dcb242166e31d00ca453c8
0x41gawor <41gawor@gmail.com>
2022-01-29 14:44:34 +0100

    chore: open model for javafx_base in order to PropertyValueFactory work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,10 @@ module com.example.hello {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.java;
 
 
     opens com.example.hello to javafx.fxml;
+    opens com.example.hello.model to javafx.base;
     exports com.example.hello;
 }
